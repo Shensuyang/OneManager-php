3ec111a95f6eb257ecbb02f9d5ca00e37866d3ef
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2021-12-17 16:58:48 +0800

    Update Vercel_env.php

--- platform/Vercel_env.php
+++ platform/Vercel_env.php
@@ -379,32 +379,49 @@ function OnekeyUpate($auth = 'qkqpttgf', $project = 'OneManager-php', $branch = 
 {
     $tmppath = '/tmp';
 
-    // 从github下载对应tar.gz，并解压
-    $url = 'https://github.com/' . $auth . '/' . $project . '/tarball/' . urlencode($branch) . '/';
+    if ($GitSource=='Github') {
+        // 从github下载对应tar.gz，并解压
+        $url = 'https://github.com/' . $auth . '/' . $project . '/tarball/' . urlencode($branch) . '/';
+    } elseif ($GitSource=='HITGitlab') {
+        $url = 'https://git.hit.edu.cn/' . $auth . '/' . $project . '/-/archive/' . urlencode($branch) . '/' . $project . '-' . urlencode($branch) . '.tar.gz';
+    } else return json_encode(['error'=>['code'=>'Git Source input Error!']]);
     $tarfile = $tmppath . '/github.tar.gz';
-    $githubfile = file_get_contents($url);
-    if (!$githubfile) return '{"error":{"message":"fail to download from github"}}';
-    file_put_contents($tarfile, $githubfile);
-        $phar = new PharData($tarfile); // need php5.3, 7, 8
-        $phar->extractTo($tmppath, null, true);//路径 要解压的文件 是否覆盖
+    file_put_contents($tarfile, file_get_contents($url));
+    $phar = new PharData($tarfile);
+    $html = $phar->extractTo($tmppath, null, true);//路径 要解压的文件 是否覆盖
     unlink($tarfile);
 
-    $outPath = '';
-    $tmp = scandir($tmppath);
-    $name = $auth . '-' . $project;
-    mkdir($tmppath . "/" . $name, 0777);
-    foreach ($tmp as $f) {
-        if ( substr($f, 0, strlen($name)) == $name) {
-            rename($tmppath . '/' . $f, $tmppath . "/" . $name . '/api');
-            $outPath = $tmppath . "/" . $name;
-            break;
-        }
-    }
+    // 获取解压出的目录名
+    $outPath = findIndexPath($tmppath);
+    if ($outPath=='') return '{"error":{"message":"no outpath"}}';
+    $name = $project . 'CODE';
+    mkdir($tmppath . "/" . $name, 0777, 1);
+    rename($outPath, $tmppath . "/" . $name . '/api');
+    $outPath = $tmppath . "/" . $name;
     //echo $outPath . "<br>";
     //error_log1($outPath);
-    if ($outPath=='') return '{"error":{"message":"no outpath"}}';
 
     return VercelUpdate(getConfig('HerokuappId'), getConfig('APIKey'), $outPath);
+}
+
+function findIndexPath($rootpath, $path = '')
+{// find the path of first 'index.php'
+    if (substr($rootpath,-1)=='/') $rootpath = substr($rootpath, 0, -1);
+    if (substr($path,0,1)=='/') $path = substr($path, 1);
+    $handler=opendir(path_format($rootpath.'/'.$path)); //打开当前文件夹
+    while($filename=readdir($handler)){
+        if($filename != "." && $filename != ".."){//文件夹文件名字为'.'和‘..’，不要对他们进行操作
+            $nowname = path_format($rootpath.'/'.$path."/".$filename);
+            if(is_dir($nowname)){// 如果读取的某个对象是文件夹，则递归
+                $res = findIndexPath($rootpath, $path."/".$filename);
+                if ($res!=='') return $res;
+            }else{
+                if ($filename==='index.php') return $rootpath.'/'.$path;
+            }
+        }
+    }
+    @closedir($path);
+    return '';
 }
 
 function WaitFunction($deployid) {
